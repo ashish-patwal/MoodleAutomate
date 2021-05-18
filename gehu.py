96c3328fc009f0c11105dd5bf02a9c3aea2ac3ea
ashish-patwal <ashishpatwal147@gmail.com>
2021-05-18 22:33:24 +0530

    modified'

--- gehu.py
+++ gehu.py
@@ -6,20 +6,16 @@ from parser import cmd_parser
 from const import URL, MAINURL, params, headers, motive
 from util import calenderEvents, listSubjects, submitAttendance
 
-# payload = {
-#        'sessid': '2542',
-#        'sesskey': 'LAKRb1WMNT',
-#        '_qf__mod_attendance_student_attendance_form': '1',
-#        'mform_isexpanded_id_session': '1',
-#        'status': '3365',
-#        'submitbutton': 'Save changes'}
-
 args = cmd_parser()
 
 if len(sys.argv) == 1:
     print('Please provide arguments')
 
 else:
+    if args.show_motive:
+        print(motive)
+        exit(0)
+
     with requests.Session() as session:
         with RequestURL(URL, session, headers) as soup:
             params['logintoken'] = soup.find(
@@ -37,10 +33,8 @@ else:
                 print('updated cookies for moodle session')
                 print('-'*20)
 
-                if args.show_motive:
-                    print(motive)
 
-                elif args.list_subjects:
+                if args.list_subjects:
                     with RequestURL(MAINURL, session, headers) as soup:
                         sesskey = soup.find(
                             'input', {'name': 'sesskey'})['value']
